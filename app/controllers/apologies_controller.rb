5ef6ed087540e681f1cf506d78940c4bccc27aad
Ian Marshall <imarshall14@gmail.com>
2021-03-08 19:23:06 -0500

    remove script tag from layout

--- app/controllers/apologies_controller.rb
+++ app/controllers/apologies_controller.rb
@@ -1,2 +1,6 @@
 class ApologiesController < ApplicationController
+
+    def index
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  # get 'home/index'
   resources :forgivenesses
   resources :apologies
   resources :incidents
   devise_for :users
   # get 'home/index'
   root to: "home#index"
+  # root to: "apologies#index"
+
 
 end
