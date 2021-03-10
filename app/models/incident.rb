6fe273c886b8f54efaa5ecbb72e4b52f930cf7c1
Ian Marshall <imarshall14@gmail.com>
2021-03-10 14:36:56 -0500

    add apologized method to Incident model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,4 +1,8 @@
 class Incident < ApplicationRecord
     has_many :apologies
     has_many :users, through: :apologies
+
+    def apologized?(user)
+        !!self.apologies.find{|apology| apology.user_id == user.id}
+    end
 end
