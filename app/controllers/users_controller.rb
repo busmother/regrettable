035c1cd37b902d8343c18a29c1c4a0b043cab056
Ian Marshall <imarshall14@gmail.com>
2021-03-10 16:36:41 -0500

    create 404 view and redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
 
     def show
-        @user = User.find_by(id: params[:id])
+        #@user = User.find_by(id: params[:id])
+        @user = User.find(params[:id])
     end
+
+
+    #create a way to throw an error if it doesn't find a user
 
 end
