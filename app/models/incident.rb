ed78e3cfd9ff898e90bc181784b42ed36ae2dce1
Ian Marshall <imarshall14@gmail.com>
2021-03-10 20:55:18 -0500

    create basic incident / apology form

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,6 +1,7 @@
 class Incident < ApplicationRecord
     has_many :apologies
     has_many :users, through: :apologies
+    accepts_nested_attributes_for :apologies
 
     def apologized?(user)
         !!self.apologies.find{|apology| apology.user_id == user.id}
