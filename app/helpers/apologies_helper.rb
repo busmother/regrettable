1e1dd8efb04577dd5cd7c74c52724681b3d8014d
Ian Marshall <imarshall14@gmail.com>
2021-03-14 15:00:58 -0400

    create show_apologizers helper method

--- app/helpers/apologies_helper.rb
+++ app/helpers/apologies_helper.rb
@@ -3,21 +3,21 @@ module ApologiesHelper
     def show_forgives(apology)
         if apology.forgiven?(current_user)
             if apology.forgives_count > 2
-                "#{link_to "You", current_user} and #{@apology.forgives_count-1} others forgive this:"
+                "#{link_to "You", current_user} and #{@apology.forgives_count-1} others forgive this"
             elsif apology.forgives_count == 2
-                "#{link_to "You", current_user} and 1 other forgive this:"
+                "#{link_to "You", current_user} and 1 other forgive this"
             elsif apology.forgives_count == 1
-                "#{link_to "You", current_user} forgive this:"
+                "#{link_to "You", current_user} forgive this"
             else
                 "No one forgives this yet - will you be the first?"
             end
         else
             if apology.forgives_count > 2
-                "#{link_to apology.first_forgive.name, apology.first_forgive} and #{@apology.forgives_count-1} other forgive this:"
+                "#{link_to apology.first_forgive.name, apology.first_forgive} and #{@apology.forgives_count-1} others forgive this"
             elsif apology.forgives_count == 2
-                "#{link_to apology.first_forgive.name, apology.first_forgive} and 1 other forgive this:"
+                "#{link_to apology.first_forgive.name, apology.first_forgive} and 1 other forgive this"
             elsif apology.forgives_count == 1
-                "#{link_to apology.first_forgive.name, apology.first_forgive} forgive this:"
+                "#{link_to apology.first_forgive.name, apology.first_forgive} forgive this"
             else
                 "No one forgives this yet - will you be the first?"
             end
@@ -32,7 +32,7 @@ module ApologiesHelper
         end
     end
 
-    def show_header
+    def show_header(apology)
         "#{link_to @apology.user.name, @apology.user} apologises for #{link_to @apology.incident.name, @apology.incident}"
     end
 
