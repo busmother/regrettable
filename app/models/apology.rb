ab366dd0a8fba4df1ef11362128c2775099f9e0a
Ian Marshall <imarshall14@gmail.com>
2021-03-15 18:28:08 -0400

    update apology validation

--- app/models/apology.rb
+++ app/models/apology.rb
@@ -3,7 +3,7 @@ class Apology < ApplicationRecord
   belongs_to :incident
   has_many :forgivenesses
   has_many :users, through: :forgivenesses
-  validates :name, length: {minimum: 2}
+  validates :body, length: {minimum: 50}
 
   def forgiven?(user)
     !!self.forgivenesses.find{|forgiveness| forgiveness.user_id == user.id}
