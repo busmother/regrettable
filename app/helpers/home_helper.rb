3cdb6d0afc1c17379547632908d438b3c75f224e
Ian Marshall <imarshall14@gmail.com>
2021-03-17 18:59:35 -0400

    update home view for when logged out

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+
+    def show_home
+        if current_user != nil
+            "<h3> Newest apologies </h3>"
+            "#{render 'apologies/index'}"
+        else
+            ""
+        end
+    end
+
 end
