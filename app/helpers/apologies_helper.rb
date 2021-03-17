3b15dd2fd9225c0f4bb55b67fb3606620c0ce7e2
Ian Marshall <imarshall14@gmail.com>
2021-03-17 10:52:59 -0400

    refactor show_apology_buttons helper method(s)

--- app/helpers/apologies_helper.rb
+++ app/helpers/apologies_helper.rb
@@ -1,9 +1,13 @@
 module ApologiesHelper
 
+    def show_header(apology)
+        "#{link_to apology.user.name, apology.user} #{link_to "apologises", apology} for #{link_to apology.incident.name, apology.incident}"
+    end
+
     def show_forgives(apology)
         if apology.forgiven?(current_user)
             if apology.forgives_count > 2
-                "#{link_to "You", current_user} and #{@apology.forgives_count-1} others forgive this"
+                "#{link_to "You", current_user} and #{apology.forgives_count-1} others forgive this"
             elsif apology.forgives_count == 2
                 "#{link_to "You", current_user} and 1 other forgive this"
             elsif apology.forgives_count == 1
@@ -15,7 +19,7 @@ module ApologiesHelper
             end
         else
             if apology.forgives_count > 2
-                "#{link_to apology.first_forgive.name, apology.first_forgive} and #{@apology.forgives_count-1} others forgive this"
+                "#{link_to apology.first_forgive.name, apology.first_forgive} and #{apology.forgives_count-1} others forgive this"
             elsif apology.forgives_count == 2
                 "#{link_to apology.first_forgive.name, apology.first_forgive} and 1 other forgive this"
             elsif apology.forgives_count == 1
@@ -28,23 +32,31 @@ module ApologiesHelper
         end
     end
 
-    def show_forgive(apology)
-        if apology.forgiven?(current_user)
-            "#{button_to 'Unforgive', forgiveness_path(apology), method: 'delete'}"
-        else
-            "#{button_to 'Forgive', forgiveness_path(apology), method: 'put'}"
-        end
-    end
+    # def show_forgive(apology)
+    #     if apology.forgiven?(current_user)
+    #         "#{button_to 'Unforgive', forgiveness_path(apology), method: 'delete'}"
+    #     else
+    #         "#{button_to 'Forgive', forgiveness_path(apology), method: 'put'}"
+    #     end
+    # end
 
-    def show_header(apology)
-        "#{link_to apology.user.name, apology.user} #{link_to "apologises", apology} for #{link_to apology.incident.name, apology.incident}"
-    end
+    # def show_edit_or_forgive(apology)
+    #     if apology.user == current_user
+    #         "#{button_to "Edit your apology", edit_apology_path(apology)} <br> #{button_to "Remove apology", apology_path(apology), method: 'delete'}"
+    #     else
+    #         show_forgive(apology)
+    #     end
+    # end
 
-    def show_edit_or_forgive(apology)
+    def show_apology_buttons(apology)
         if apology.user == current_user
             "#{button_to "Edit your apology", edit_apology_path(apology)} <br> #{button_to "Remove apology", apology_path(apology), method: 'delete'}"
         else
-            show_forgive(apology)
+            if apology.forgiven?(current_user)
+                "#{button_to 'Unforgive', forgiveness_path(apology), method: 'delete'}"
+            else
+                "#{button_to 'Forgive', forgiveness_path(apology), method: 'put'}"
+            end
         end
     end
 
