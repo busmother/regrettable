3b0d9d14c361f0a59aa1d8746eba14292f906c10
Ian Marshall <imarshall14@gmail.com>
2021-07-28 11:33:01 -0400

    update db from sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_03_14_193023) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "apologies", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
